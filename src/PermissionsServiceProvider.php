a5db2802442c602c1176fb53902f97c578c62ec6
Denis Mitrofanov <denis.mitr@gmail.com>
2018-06-05 18:26:39 +0300

    changes service provider

--- src/PermissionsServiceProvider.php
+++ src/PermissionsServiceProvider.php
@@ -16,24 +16,24 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        if ( ! is_lumen()) {
+            $this->publishes([
+                __DIR__.'/../config/permissions.php' => config_path('permissions.php'),
+            ], 'config');
+
+            $this->publishMigrations();
+        }
 		
 		
 		try {			
-			Permission::get()->map(function ($permission) {
-				Gate::define($permission->name, function ($user) use ($permission) {
-					return $user->hasPermissionTo($permission);
-				});
-			});
+//			Permission::get()->map(function ($permission) {
+//				Gate::define($permission->name, function ($user) use ($permission) {
+//					return $user->hasPermissionTo($permission);
+//				});
+//			});
 		} catch(\Throwable $t) {}
 
-        Blade::directive('role', function($role) {
-            return "<?php if(auth()->check() && auth()->user()->hasRole({$role})): ?>";
-        });
 
-        Blade::directive('endrole', function() {
-            return "<?php endif; ?>";
-        });
     }
 
     /**
@@ -43,6 +43,42 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if ( ! is_lumen()) {
+            $this->mergeConfigFrom(
+                __DIR__ . './../config/permissions.php',
+                'permissions'
+            );
+        }
 
+        $this->registerBladeDirectives();
+    }
+
+    protected function registerBladeDirectives()
+    {
+        Blade::directive('role', function($role) {
+            return "<?php if(auth()->check() && auth()->user()->hasRole({$role})): ?>";
+        });
+
+        Blade::directive('endrole', function() {
+            return "<?php endif; ?>";
+        });
+    }
+
+    protected function publishMigrations()
+    {
+        $timestamp = date('Y_m_d_His', time());
+
+        $this->publishes([
+            __DIR__ . './../migrations/create_roles_table.php' =>
+                database_path("/migrations/{$timestamp}_create_roles_table.php"),
+            __DIR__ . './../migrations/create_permissions_table.php' =>
+                database_path("/migrations/{$timestamp}_create_permissions_table.php"),
+            __DIR__ . './../migrations/create_user_roles_table.php' =>
+                database_path("/migrations/{$timestamp}_create_user_roles_table.php"),
+            __DIR__ . './../migrations/create_user_permissions_table.php' =>
+                database_path("/migrations/{$timestamp}_create_user_permissions_table.php"),
+            __DIR__ . './../migrations/create_role_permissions_table.php' =>
+                database_path("/migrations/{$timestamp}_create_role_permissions_table.php"),
+        ]);
     }
 }
